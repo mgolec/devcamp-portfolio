a691b09dbf039fe09d0514e59eb8480cbf7f7587
Marko Golec <dngy@Markos-iMac.local>
2018-04-10 14:33:47 +0200

    added delete option for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -42,4 +42,14 @@ def update
     @portfolio_item = Portfolio.find(params[:id])
   end
 
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+
+    @portfolio_item.destroy
+
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully destroyed.' }
+    end
+  end
+
 end
